2538b944f0fc3767c194540f2d8e6f02e333293e
BeneX Drake <benex@hotmail.de>
2025-01-02 15:34:41 +0100

    remove comment

--- js/components/card.js
+++ js/components/card.js
@@ -3,7 +3,6 @@ export let cardBlock = async (anime) =>
     let card = document.createElement('div')
     card.className = 'card';
     card.style.backgroundImage = `url(${anime.imageUrl})`
-    // console.log(anime.imageUrl)
 
     return card;
 }
